e5cbcdcf07a4e8c3aa93ea627a706cbb8c5bee67
Chiwei Chang <cchang@HX5NYC.ORG>
2018-08-09 11:17:10 -0400

    Add eaten pizza state

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -3,12 +3,36 @@ import pizza from '../img/pizza.png';
 import './Pizza.css';
 
 export default class Pizza extends Component {
+  state = {
+    eaten: null
+  }
+  makePizzaEaten = () => {
+    this.setState(() => ({
+      eaten: this.props.yourTurn ? 'byYou' : 'byEnemy'
+    }));
+  }
+  handleMakePizzaEatenAndEatPizza = (weightToAddNext) => {
+    this.props.eatPizza(this.props.size, weightToAddNext, this.props.id);
+    this.makePizzaEaten();
+  }
   render() {
     let weightToAddNext = this.props.yourTurn ? 'yourWeight' : 'enemyWeight';
 
-    return (<div onClick={() => this.props.eatPizza(this.props.size, weightToAddNext, this.props.id)}>
-              <img className="pizza" src={pizza} alt="pizza" />
-              <p className="dark-bold">{this.props.size}</p>
-            </div>)
+    if (this.state.eaten === null) {
+      return (<div onClick={() => this.handleMakePizzaEatenAndEatPizza(weightToAddNext)}>
+                <img className="pizza" src={pizza} alt="pizza" />
+                <p className="dark-bold">{this.props.size}</p>
+              </div>)
+    } else if (this.state.eaten === 'byYou') {
+      return (<div>
+                <img className="pizza pizza-blue" src={pizza} alt="pizza" />
+                <p className="dark-bold">{this.props.size}</p>
+              </div>)
+    } else if (this.state.eaten === 'byEnemy') {
+      return (<div>
+                <img className="pizza pizza-red" src={pizza} alt="pizza" />
+                <p className="dark-bold">{this.props.size}</p>
+              </div>)
+    }
   }
 }
