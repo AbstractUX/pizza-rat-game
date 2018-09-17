3062e3ed73fdf50b33a83050cf17a978ffe8766a
Chiwei Chang <cchang@HX5NYC.ORG>
2018-09-17 15:07:39 -0400

    Add link to AbstractUX

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <span className="left position-absolute">Game by <a href="http://AbstractUX.com">AbstractUX</a></span>
           <img src={pizza} className="App-logo" alt="pizza logo" />
           <h1 className="App-title">Welcome to the Pizza Rat Game</h1>
         </header>
